572757a83faea924dfb56476597dfa5242ce1e0e
teeteathao <87237312+teeteathao@users.noreply.github.com>
2021-10-16 13:48:34 -0700

    create function for colors for past present future

--- Develop/script.js
+++ Develop/script.js
@@ -10,11 +10,32 @@ var date = setInterval(() => {
 },1000)
 
 // Timeblocks
+// create function that checks the current time to see past, present, future
 function hourTrack(){
   var currentHour = moment().hour();
     console.log(currentHour);
   
+  $(".row").each(function(){
+    var hourColor =parseInt($(this).attr("id"))
+    console.log (hourColor, currentHour)
+
+    if(currentHour > hourColor){
+      $(this).addClass("past");
+    }
+    else if(currentHour === hourColor){
+      $(this).removeClass("past");
+      $(this).addClass("present"); 
+      $(this).removeClass("future");
+    }
+    else{
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  })
 }
+hourTrack()
+
 
 // Add eventlistener to saveBtn
 $(".saveBtn").on("click", function(){
@@ -30,9 +51,26 @@ $(".saveBtn").on("click", function(){
   localStorage.setItem(userInput, timeBlock)
 })
 
-var obj = {
-  firstName: "thao", 
-  "firstName": "thao", 
-}
+// get item = persist to keep item on the page when refreshed
+
+$("#hour-8am .userInput").val(localStorage.getItem("hour-8am"));
+$("#hour-9am .userInput").val(localStorage.getItem("hour-9am"));
+$("#hour-10am .userInput").val(localStorage.getItem("hour-10am"));
+$("#hour-11am .userInput").val(localStorage.getItem("hour-11am"));
+$("#hour-12pm .userInput").val(localStorage.getItem("hour-12pm"));
+$("#hour-1pm .userInput").val(localStorage.getItem("hour-1pm"));
+$("#hour-2pm .userInput").val(localStorage.getItem("hour-2pm"));
+$("#hour-3pm .userInput").val(localStorage.getItem("hour-3pm"));
+$("#hour-4pm .userInput").val(localStorage.getItem("hour-4pm"));
+$("#hour-5pm .userInput").val(localStorage.getItem("hour-5pm"));
+
+
+
+
+
+
 
 })
+
+
+// objects, json, data types, event delegation, event propagation
