75c87099579ebc64855f781445a87b08e57afb58
teeteathao <87237312+teeteathao@users.noreply.github.com>
2021-10-16 09:22:11 -0700

    add eventlistener to button

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,38 @@
+$(document).ready(function (){
+
 var dateEl = $('#currentDay');
 
 
-
-// Refresh current time each second
+// Display current date
 dateEl.text(moment().format("dddd MMMM DD, YYYY"))
 var date = setInterval(() => {
   dateEl.text(moment().format("dddd MMMM DD, YYYY"))
 },1000)
+
+// Timeblocks
+function hourTrack(){
+  var currentHour = moment().hour();
+    console.log(currentHour);
+  
+}
+
+// Add eventlistener to saveBtn
+$(".saveBtn").on("click", function(){
+  console.log("text")
+  // grab input
+  var userInput = $(this).siblings(".userInput").val();
+  console.log(typeof userInput)
+  // check timeblock 11 class
+  // grab time
+  var timeBlock = $(this).parent().attr("id");
+  console.log(typeof timeBlock)
+  // save to local storage
+  localStorage.setItem(userInput, timeBlock)
+})
+
+var obj = {
+  firstName: "thao", 
+  "firstName": "thao", 
+}
+
+})
